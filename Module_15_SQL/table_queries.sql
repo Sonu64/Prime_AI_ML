07eae5bd99b7415891cd2d44421f1ac10aa186d6
Sonu64 <sonusantu64@gmail.com>
2026-02-20 20:08:43 +0530

    Update table_queries.sql to include SELECT statements and clarify table queries. Checked Erros and Default value handlings and also learnt DISTINCT keyword.

--- Module_15_SQL/table_queries.sql
+++ Module_15_SQL/table_queries.sql
@@ -1,4 +1,4 @@
-/* Table Queries -> CREATE, INSERT, UPDATE, ALTER, TRUNCATE, DELETE */
+/* Table Queries -> CREATE, SELECT, INSERT, UPDATE, ALTER, TRUNCATE, DELETE */
 /* Common Constraints -> UNIQUE, NOT NULL, DEFAULT, CONSTRAINT CHECK (condition) */
 /* Key Constraints -> PRIMARY KEY, FOREIGN KEY */
 
@@ -21,6 +21,39 @@ CREATE TABLE post (
     user_id INT,
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
+
+/* Default values used for followers and following */
+INSERT INTO user 
+(id, age, username, email)
+VALUES
+(100, 23, "sonu.codes", "sonu@email.com");
+
+/* Duplication Error if email already exists */
+INSERT INTO user 
+(id, age, username, email)
+VALUES
+(101, 45, "hanuman.codes", "sonu@email.com");
+
+/* Constraint check error if age < 13, email not given */
+INSERT INTO user 
+(id, age, username)
+VALUES
+(102, 3, "priyanka.codes");
+
+/* Default email value */
+INSERT INTO user 
+(id, age, username)
+VALUES
+(101, 23, "bubu_bankura");
+
+/* Show specific columns from a table */
+SELECT id, username FROM user;
+/* Show all columns/rows from a table */
+SELECT * FROM user;
+
+/* DISTINCT keyword */
+SELECT DISTINCT (age), (id), (username) FROM user; /*(entire group is distinct )*/
+SELECT DISTINCT (age) FROM user; /*(entire group is distinct -> Here the group contains only the age column )*/
 
 
 
